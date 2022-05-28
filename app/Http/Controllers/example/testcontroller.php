0377f0acb511440d3609277d32490c3806f9a525
Nayem Ishan <nayemishan377@gmail.com>
2022-05-29 00:53:54 +0600

    controller basic added

---
+++ app/Http/Controllers/example/testcontroller.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\example;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class testcontroller extends Controller
+{
+    public function index(){
+        return view('welcome');
+    }
+
+    public function nayem(){
+        return view('nayem');
+    }
+
+    public function hossain(){
+        return view('hossain');
+    }
+
+    public function ishan(){
+        return view('ishan');
+    }
+
+    public function creator(){
+        return view('ourcreator');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\example\testcontroller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,25 +13,51 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*right way to define view file using route who will send them in controller */
+
+//index page routing
+Route::get('/', [testcontroller::class, 'index']);
+//nayem page routing
+Route::get('/nayem', [testcontroller::class, 'nayem']);
+//hossain page routing
+Route::get('/hossain', [testcontroller::class, 'hossain']);
+//ishan page routing
+Route::get('/ishan', [testcontroller::class, 'ishan']);
+//named routing
+Route::get('/creator', [testcontroller::class, 'creator'])->name('mycreator');
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* this is wrong way to define view file by rout */
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 
 // // One Way to define route
-Route::get('/nayem',function(){
-        return view('nayem');
-    });
+// Route::get('/nayem',function(){
+//         return view('nayem');
+//     });
  
     
-// Another Way to define route
-Route::view('/hossain','hossain');
-
-
 // // Another Way to define route
-Route::match(['get','post'], '/ishan', function(){
-    return view('ishan');
-});
+// Route::view('/hossain','hossain');
+
+
+// // // Another Way to define route
+// Route::match(['get','post'], '/ishan', function(){
+//     return view('ishan');
+// });
 
 // // Another Way to define route
 // Route::any('/contact', function(){
@@ -39,15 +65,15 @@ Route::match(['get','post'], '/ishan', function(){
 // });
 
 
-// // parameter routing
-Route::any('/contact/{serial}', function($serial){
-    return "I need you $serial serial number";
-});
+// // // parameter routing
+// Route::any('/contact/{serial}', function($serial){
+//     return "I need you $serial serial number";
+// });
 
-// // named routing
-Route::get('/new', function(){
-    return "Allah is our creator";
-})->name('mycreator');
+// // // named routing
+// Route::get('/new', function(){
+//     return "Allah is our creator";
+// })->name('mycreator');
 
 
 //route using middleware
