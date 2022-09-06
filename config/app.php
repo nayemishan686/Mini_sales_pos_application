ddd38800c80d0136e2c66acfc9ede616b1d8b0ed
Nayem Ishan <nayemishan377@gmail.com>
2022-09-06 22:12:00 +0600

    Alhamdulillah require package install successfully

--- config/app.php
+++ config/app.php
@@ -185,6 +185,8 @@ return [
         /*
          * Package Service Providers...
          */
+        Yajra\DataTables\DataTablesServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -210,6 +212,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Image' => Intervention\Image\Facades\Image::class
     ])->toArray(),
 
 ];
